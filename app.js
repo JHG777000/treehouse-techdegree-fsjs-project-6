b1c2e41280e5696b15a2670b58ad7e128f28ad88
JHG777000 <jhg777000@gmail.com>
2020-04-19 10:44:13 -0400

    add info to data.json

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const data = require('./data.json');
 const app = express();
 
 app.set('view engine', 'pug');
 
+app.use('/static',express.static('public'));
+
 app.get('/',(req,res) => {
-    res.render('index');
+    res.render('index',data);
+});
+
+app.get('/about',(req,res) => {
+    res.render('about');
+});
+
+app.get('/project/:id',(req,res) => {
+    res.render('project');
 });
 
 app.listen(3000, () => {
