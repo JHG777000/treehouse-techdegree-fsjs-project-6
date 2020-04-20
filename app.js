db40351046c1ec77af60f5e9422a2b633886c267
JHG777000 <jhg777000@gmail.com>
2020-04-20 08:24:10 -0400

    add error handling to app.js

--- app.js
+++ app.js
@@ -14,10 +14,29 @@ app.get('/about',(req,res) => {
     res.render('about');
 });
 
-app.get('/project/:id',(req,res) => {
+app.get('/project/:id',(req,res,next) => {
+    if ( req.params.id >= data.projects.length 
+        || req.params.id < 0 
+        || !Number.isInteger(parseInt(req.params.id))) {
+        const err =  new Error('No project with that id exists.');
+        err.status = 500;
+        return next(err);
+    }
     res.render('project',{
         project: data.projects[req.params.id]
     });
+});
+
+app.use((req,res,next) => {
+    const err =  new Error('The page you requested, can not be found.');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err,req,res,next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 app.listen(3000, () => {
